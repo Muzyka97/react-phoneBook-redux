658ac64348a47737c6b9da0993608086e0453a51
Kateryna Muzyka <katerynamuzyka@MacBook-Air-Kateryna.local>
2022-11-18 12:48:57 +0200

    with update and mount

--- src/components/PhoneBook/PhoneBook.jsx
+++ src/components/PhoneBook/PhoneBook.jsx
@@ -70,6 +70,20 @@ class PhoneBook extends Component{
         })
         return filterContacts;
     };
+    componentDidMount(){
+        const contacts = localStorage.getItem("contacts");
+        const parsedContacts = JSON.parse(contacts);
+        // parsedContacts?.length like default value.
+        if(parsedContacts?.length){
+            this.setState({contacts: parsedContacts})
+        };
+    };
+
+    componentDidUpdate(prevProps, prevState){
+        if(this.state.contacts !== prevState.contacts){
+            localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
+        };
+    };
 
     render(){
      const {filter} = this.state
